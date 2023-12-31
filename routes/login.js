25799ed0648eabdfc0ed67819dfc5201ac6f71e1
DarShan Pandya <itznotabug@gmail.com>
2023-12-31 16:06:30 +0530

    Redirect user to specified page (if exists) after login.

--- routes/login.js
+++ routes/login.js
@@ -4,13 +4,17 @@ import Miscellaneous from '../utils/misc.js';
 const router = express.Router();
 
 router.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login', {redirect: req.query.redirect ?? ''});
 });
 
 router.post('/login', async (req, res) => {
     const authStatus = await Miscellaneous.authenticated(req);
-    if (authStatus.level === 'success') return res.redirect('/');
-    else return res.render('login', authStatus);
+    if (authStatus.level === 'success') {
+        let redirect = req.body.redirect;
+        if (!redirect) redirect = '/';
+
+        return res.redirect(redirect);
+    } else return res.render('login', authStatus);
 });
 
 router.get('/logout', (req, res) => {
