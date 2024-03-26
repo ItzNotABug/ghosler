feb55ffb218b84983e7a360fd01f4440580a6905
ItzNotABug <itznotabug@gmail.com>
2024-03-26 14:19:05 +0530

    fix: misc.

--- docker-install.sh
+++ docker-install.sh
@@ -10,13 +10,13 @@ DEFAULT_PORT=2369
 DEFAULT_CONTAINER_NAME="ghosler"
 
 # Prompt for port number with default
-echo -n "Select a port for Ghosler [Default: $DEFAULT_PORT] >> "
-read\ PORT
+echo -e "${GREEN}Provide a port for Ghosler [Default: $DEFAULT_PORT]${NC} : "
+read -r PORT
 PORT=${PORT:-$DEFAULT_PORT}
 
 # Prompt for container name with default
-echo -n "Select a name for Ghosler Container [Default: $DEFAULT_CONTAINER_NAME] >> "
-read\ CONTAINER_NAME
+echo -e "${GREEN}Provide a name for Ghosler Container [Default: $DEFAULT_CONTAINER_NAME]${NC} : "
+read -r CONTAINER_NAME
 CONTAINER_NAME=${CONTAINER_NAME:-$DEFAULT_CONTAINER_NAME}
 
 echo -e "${GREEN}Starting Ghosler Docker installation...${NC}"
@@ -25,9 +25,9 @@ echo ""
 # Use the variables in the docker run command
 if ! docker run --rm --name "$CONTAINER_NAME" -d -p "$PORT":2369 -v "${CONTAINER_NAME}"-logs:/usr/src/app/.logs -v "${CONTAINER_NAME}"-analytics:/usr/src/app/files -v "${CONTAINER_NAME}"-configuration:/usr/src/app/configuration itznotabug/ghosler:0.94; then
     echo ""
-    echo -e "${RED}Error: Failed to start $CONTAINER_NAME. Please check the Docker logs for more details.${NC}" >&2
+    echo -e "${RED}Error: Failed to start Ghosler. Please check the Docker logs for more details.${NC}" >&2
     exit 1
 fi
 
 echo ""
-echo -e "${GREEN}$CONTAINER_NAME has been successfully installed and started.${NC}"
+echo -e "${GREEN}Ghosler has been successfully installed and started.${NC}"
